e775de2c71d3cb30dd8f59cb24427cd290851124
Apres Azghaldian <azgaldian2@gmail.com>
2022-08-09 02:08:55 +0300

    home page fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App-header">
         <Router>
           <Routes>
-            <Route path='/' element={<SignInArea />} />
+            <Route path='/styled-component' element={<SignInArea />} />
             <Route path='/sign-up' element={<SignUpArea />} />
           </Routes>
         </Router>
